47b3fc1725afdfea8bf1f7f1a4df515dc9db2bcf
NeJan <5615449+NeJan@user.noreply.gitee.com>
2020-10-30 11:45:20 +0800

    Initial commit

---
+++ src/main/java/web/qingmu/qingmubackend/QingmuBackendApplication.java
@@ -0,0 +1,13 @@
+package web.qingmu.qingmubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingmuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingmuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/web/qingmu/qingmubackend/QingmuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package web.qingmu.qingmubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingmuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
